88f6fddeee922ed1c5cd104b4ac171c7626e2796
SakeDon <dhgi0630@gmail.com>
2017-03-07 14:11:26 +0900

    Update computeCost.m

--- Andrew Ng/ex1/computeCost.m
+++ Andrew Ng/ex1/computeCost.m
@@ -13,7 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
+predictions = X * theta;
+J = sum((predictions - y).^2)/(2*m);
 
 
 
